906062d3976b53d698c595088c1f05021a1a170e
Tobias <tobias.weinlich@gmx.de>
2023-06-03 20:27:38 +0200

    Added Reading array for monitoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Reading from 'src/reading/reading.class';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BloodPressure';
+
+  readings: Reading[] = new Array<Reading>();
+
+  updateRecord(readings: Reading[]) {
+    const newReadings = new Array<Reading>();
+
+    for (const reading of readings) {
+      newReadings.push(new Reading(reading.systolic, reading.diastolic, reading.pulse, reading.date, reading.time, reading.notes));
+    }
+
+    this.readings = newReadings;
+  }
 }
