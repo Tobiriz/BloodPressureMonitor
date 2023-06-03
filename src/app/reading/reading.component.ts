a801e538d71e6a729c3a0bb02643eb0c975e312c
Tobias <tobias.weinlich@gmx.de>
2023-06-03 20:34:12 +0200

    Added Inputs

--- src/app/reading/reading.component.ts
+++ src/app/reading/reading.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Prognosis } from 'src/reading/reading.class';
 
 @Component({
   selector: 'app-reading',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./reading.component.scss']
 })
 export class ReadingComponent {
+
+  @Input() systolic: number | undefined;
+  @Input() diastolic: number | undefined;
+  @Input() pulse: number | undefined;
+  @Input() date: string | undefined;
+  @Input() time: string | undefined;
+  @Input() notes: string | undefined;
+  @Input() prognosis: Prognosis | undefined;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
