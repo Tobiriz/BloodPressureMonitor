ca06e73cc6caf32f42baedd18fedf69bc0911fd8
Tobias <tobias.weinlich@gmx.de>
2023-06-03 13:24:29 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MonitorComponent } from './monitor/monitor.component';
+import { ReadingComponent } from './reading/reading.component';
+import { RecordComponent } from './record/record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitorComponent,
+    ReadingComponent,
+    RecordComponent
   ],
   imports: [
     BrowserModule
