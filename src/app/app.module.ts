1f2090f06fc46e9160912f27d7f20e389df5235a
Tobias <tobias.weinlich@gmx.de>
2023-06-03 14:14:12 +0200

    Added new Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MonitorComponent } from './monitor/monitor.component';
 import { ReadingComponent } from './reading/reading.component';
 import { RecordComponent } from './record/record.component';
+import { NewReadingComponent } from './new-reading/new-reading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonitorComponent,
     ReadingComponent,
-    RecordComponent
+    RecordComponent,
+    NewReadingComponent
   ],
   imports: [
     BrowserModule
